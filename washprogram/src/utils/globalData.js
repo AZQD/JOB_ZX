d26177065c5a2867c1fd53d6f51e0cece8f5a527
liuchaojie <liuchaojie@58ganji.com>
2018-10-01 21:51:15 +0800

    正则校验

--- washprogram/src/utils/globalData.js
+++ washprogram/src/utils/globalData.js
@@ -1,3 +1,4 @@
 export const globalData = {
-  domin: 'https://xihuo.oss-cn-qingdao.aliyuncs.com/xihuo/miniprogram'
+  domin: 'https://xihuo.oss-cn-qingdao.aliyuncs.com/xihuo/miniprogram',
+  regPhone: /^1[3-9]{1}[\d]{9}$/,
 }
