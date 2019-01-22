544e8021278fd726043c95e0661dc43b690aff1e
liuchaojie <liuchaojie@58ganji.com>
2019-01-22 22:37:15 +0800

    ossDomain

--- washprogram/src/utils/globalData.js
+++ washprogram/src/utils/globalData.js
@@ -1,4 +1,4 @@
 export const globalData = {
-  domin: 'https://xihuo.oss-cn-qingdao.aliyuncs.com/xihuo/miniprogram',
+  ossDomain: 'https://xihuo.oss-cn-qingdao.aliyuncs.com/xihuo/miniprogram',
   regPhone: /^1[3-9]{1}[\d]{9}$/,
 }
