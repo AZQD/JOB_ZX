32b48ef530864762721bb02092228fac97e731b5
liuchaojie <liuchaojie@58ganji.com>
2019-01-23 18:31:12 +0800

    price-data

--- washProgram/src/utils/api.js
+++ washProgram/src/utils/api.js
@@ -1,9 +1,17 @@
 const { globalData } = require("./globalData");
 const {ajaxDomain} = globalData;
 export const  api = {
+
+  //登录注册
   AUTH_LOGIN: ajaxDomain+'/Auth/login', //用户注册接口
+
+  //首页
   GET_INDEX_CLOTH_CATEGORY: ajaxDomain+'/index/getIndexClothCategory', //获取小程序首页分类信息
   GET_WEAPP_BANNER: ajaxDomain+'/WeappBanner/getWeappBanner', //获取小程序Banner列表
 
+  //订单
   ORDER_LIST: ajaxDomain+'/User/orderList', //用户订单列表接口
+
+  //价格表
+  GET_CLOTH_CATEGORY: ajaxDomain+'/index/getClothCategory', //价格表
 };
