c4b0920be7f0145be4385f5a82fa6214b1233eda
liuchaojie <liuchaojie@58ganji.com>
2019-01-23 18:03:44 +0800

    order-data

--- washProgram/src/utils/api.js
+++ washProgram/src/utils/api.js
@@ -1,7 +1,9 @@
 const { globalData } = require("./globalData");
 const {ajaxDomain} = globalData;
 export const  api = {
-  AUTH_LOGIN: ajaxDomain+'/Auth/login',
-  GET_INDEX_CLOTH_CATEGORY: ajaxDomain+'/index/getIndexClothCategory',
-  GET_WEAPP_BANNER: ajaxDomain+'/WeappBanner/getWeappBanner',
+  AUTH_LOGIN: ajaxDomain+'/Auth/login', //用户注册接口
+  GET_INDEX_CLOTH_CATEGORY: ajaxDomain+'/index/getIndexClothCategory', //获取小程序首页分类信息
+  GET_WEAPP_BANNER: ajaxDomain+'/WeappBanner/getWeappBanner', //获取小程序Banner列表
+
+  ORDER_LIST: ajaxDomain+'/User/orderList', //用户订单列表接口
 };
