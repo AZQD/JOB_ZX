51e9a6837c0474981337b180214b9d4f4be3c34b
liuchaojie <liuchaojie@58ganji.com>
2019-01-23 17:28:55 +0800

    index-data

--- washProgram/src/utils/api.js
+++ washProgram/src/utils/api.js
@@ -2,4 +2,6 @@ const { globalData } = require("./globalData");
 const {ajaxDomain} = globalData;
 export const  api = {
   AUTH_LOGIN: ajaxDomain+'/Auth/login',
+  GET_INDEX_CLOTH_CATEGORY: ajaxDomain+'/index/getIndexClothCategory',
+  GET_WEAPP_BANNER: ajaxDomain+'/WeappBanner/getWeappBanner',
 };
