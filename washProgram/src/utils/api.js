283096a20ae2da9aa6bc5edf881228854bf6b83f
liuchaojie <liuchaojie@58ganji.com>
2019-01-23 18:39:12 +0800

    mine-data

--- washProgram/src/utils/api.js
+++ washProgram/src/utils/api.js
@@ -5,6 +5,9 @@ export const  api = {
   //登录注册
   AUTH_LOGIN: ajaxDomain+'/Auth/login', //用户注册接口
 
+  //个人中心
+  USER_DETAIL: ajaxDomain+'/User/userDetail', //我的（个人基本信息接口）
+
   //首页
   GET_INDEX_CLOTH_CATEGORY: ajaxDomain+'/index/getIndexClothCategory', //获取小程序首页分类信息
   GET_WEAPP_BANNER: ajaxDomain+'/WeappBanner/getWeappBanner', //获取小程序Banner列表
