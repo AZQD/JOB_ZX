7f05c7acbaaa6970e6a4a90d8549d91fd45217ab
AZQD <604972395@qq.com>
2024-02-27 22:17:02 +0800

    初始化PC首页顶部

--- JapanWebsite/src/views/Home.vue
+++ JapanWebsite/src/views/Home.vue
@@ -1,19 +1,71 @@
 <template>
-  <div class="home">
-<!--    <fc-designer ref="designer"/>-->
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="containerPC">
+    <div class="topBox">
+      <div class="nav">
+        <div class="left">
+          <img class="logo" src="../assets/logo.png" />
+        </div>
+        <div class="right">
+          <img class="link" src="../assets/logo.png" />
+          <img class="link" src="../assets/logo.png" />
+        </div>
+      </div>
+      <div class="info">
+        <div class="desc1">a手动阀手动阀收到罚单手动阀<br/>手动阀拉萨的风</div>
+        <div class="desc2">a手动阀手动阀手动阀拉萨的风</div>
+        <div class="desc3">a手动阀手动阀手动阀拉萨的风</div>
+      </div>
+      <div class="form">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.containerPC {
+  .topBox{
+    width: 100%;
+    height: 200px;
+    background: url("../assets/images/Mac/01.jpg") no-repeat center center;
+    background-size: 100% 100%;
+    .nav{
+      display: flex;
+      align-items: center;
+      justify-content: space-around;
+      .left {
+        .logo {
+          width: 200px;
+          height: 100px;
+        }
+      }
+      .right{
+        background-color: red;
+        display: flex;
+        .link{
+          width: 16px;
+          height: 16px;
+        }
+      }
+    }
+    .info{
+      color: #fff;
+      text-align: center;
+    }
+    .form{
+
+    }
+  }
+}
+
+</style>
